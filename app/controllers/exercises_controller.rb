37f79c39ce1df75a0f1edb5e0ff6a47260c28fdd
Sourav Das <sourav.das@quantuminventions.com>
2018-09-04 16:03:03 +0530

    Implement editing exercise

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -22,18 +22,18 @@ class ExercisesController < ApplicationController
     end
   end
   
-  # def edit
-  # end
+  def edit
+  end
   
-  # def update
-  #   if @exercise.update(exercise_params)
-  #     flash[:success] = "Exercise has been updated"
-  #     redirect_to [current_user, @exercise]
-  #   else
-  #     flash[:danger] = "Exercise has not been updated"
-  #     render :edit
-  #   end
-  # end
+  def update
+    if @exercise.update(exercise_params)
+      flash[:success] = "Exercise has been updated"
+      redirect_to [current_user, @exercise]
+    else
+      flash[:danger] = "Exercise has not been updated"
+      render :edit
+    end
+  end
   
   def show
   end
--- spec/features/listing_exercise_spec.rb
+++ spec/features/listing_exercise_spec.rb
@@ -5,8 +5,8 @@ RSpec.feature "Listing Exercises" do
     @john = User.create(email: "john@example.com", password: "password")
     login_as(@john)
 
-    @e1 = @john.exercises.create(duration_in_min: 20, workout: "Body building routine", workout_date: "2018-08-29")
-    @e2 = @john.exercises.create(duration_in_min: 20, workout: "Cardio", workout_date: "2018-08-30")
+    @e1 = @john.exercises.create(duration_in_min: 20, workout: "Body building routine", workout_date: Date.today)
+    @e2 = @john.exercises.create(duration_in_min: 20, workout: "Cardio", workout_date: Date.today)
   end
 
   scenario "shows user's workout for last 7 days" do
