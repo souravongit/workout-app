5046e681ac2ff2d273a5948bcd76a9c495c5e71e
Sourav Das <sourav.das@quantuminventions.com>
2018-09-19 15:05:34 +0530

    Implement follow user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :exercises
+  has_many :friendships
+  has_many :friends, through: :friendships, class_name: "User"
   validates :first_name, presence: true
   validates :last_name, presence: true
   self.per_page = 10 
@@ -22,5 +24,9 @@ class User < ActiveRecord::Base
       where('first_name LIKE ? or first_name LIKE ? or last_name LIKE ? or last_name LIKE ?',
         "%#{names_array[0]}%", "%#{names_array[1]}%", "%#{names_array[0]}%", "%#{names_array[1]}%").order(:first_name)
     end
+  end
+
+  def follow_or_same?(new_friend)
+    friendships.map(&:friend).include?(new_friend) || self == new_friend 
   end
 end
